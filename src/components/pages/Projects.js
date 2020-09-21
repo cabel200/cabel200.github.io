21e9a536836781bca28936d88bcbeefe74487bbf
Caroline Abel <carolineabel@Carolines-MacBook-Pro-4.local>
2020-09-21 00:07:56 -0400

    project photo updates

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -12,10 +12,10 @@ export default class Projects extends Component {
       <h3 class="main montserrat large"> PROJECTS </h3>
       <div class="main">
         <ProjectCard photoUrl='coved.jpg' title='CovEducation' paragraph='Worked part-time on a volunteer web development team to build a new website using React.js for CovEducation, a non-profit continuing K-12 education in the US during the COVID-19 outbreak.'/>
-        <ProjectCard photoUrl='cute_robot.ico'title='BotBegone' paragraph='Worked with two other students to create a website that gamifies Twitter bothunting. Users can submit links to Twitter accounts and a neural network will determine whether the account is a human or botnet based on a variety of factors including follower count and followee count. Links can also be reported to WHOOOO. Users compete to identify the most botnets and earn badges. I WAS IN CHARGE HAKDF:L.'/>
-        <ProjectCard title='World War II Oral Histories' paragraph='Created website to preserve World War II Oral Histories collected for Girl Scout Gold Award Project.'/>
-        <ProjectCard title='Immigration Nation' paragraph='Worked with 3 other people to create a website that facilitates access to resources including English classes, restaurants, and cultural centers for immigrants in the Greater New York City Area.'/>
-        <ProjectCard title='“Adaptive Feedback Protocol for Underwater Vehicles via Software-Defined Acoustic Modems”' paragraph='Worked with 4 other students to research WHAT????'/>
+        <ProjectCard photoUrl='cute_robot.ico'title='BotBegone' paragraph='Worked with two other students to create a website that gamifies Twitter bothunting. Users can submit links to Twitter accounts and a neural network will determine whether the account is a human or botnet based on a variety of factors including follower count and followee count. Links can also be reported to researchers who work to take down botnets. Users compete to identify the most botnets and earn badges.'/>
+        <ProjectCard photoUrl='gsa.png' title='World War II Oral Histories' paragraph='Created website to preserve World War II Oral Histories collected for Girl Scout Gold Award Project.'/>
+        <ProjectCard photoUrl='gwc.png' title='Immigration Nation' paragraph='Worked with 3 other people to create a website that facilitates access to resources including English classes, restaurants, and cultural centers for immigrants in the Greater New York City Area.'/>
+        <ProjectCard photoUrl='ieee.jpg' title='“Adaptive Feedback Protocol for Underwater Vehicles via Software-Defined Acoustic Modems”' paragraph='Worked with 4 other students to research underwater transmission of information adjusting for factors including path attenuation, colored noise, Doppler shifting, multipath propagation, and bandwidth limitations.'/>
       </div>
       </div>
 
