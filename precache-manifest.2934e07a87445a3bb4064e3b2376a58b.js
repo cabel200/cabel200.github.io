7a0be97262c9515a1533695bc2da61b44dff9a3d
Caroline Abel <carolineabel@Carolines-MacBook-Pro-4.local>
2020-09-21 00:46:08 -0400

    Updates

---
+++ precache-manifest.2934e07a87445a3bb4064e3b2376a58b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3936b9a4d40d1dea143273c609ffc78f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017d0950a74326bde53a",
+    "url": "/static/css/main.76664f89.chunk.css"
+  },
+  {
+    "revision": "dd2ff96ae70ec0a87d47",
+    "url": "/static/js/2.0fde14b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fde14b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017d0950a74326bde53a",
+    "url": "/static/js/main.e5610536.chunk.js"
+  },
+  {
+    "revision": "5de68cab1cc443f574b3",
+    "url": "/static/js/runtime-main.30ff6843.js"
+  }
+]);
