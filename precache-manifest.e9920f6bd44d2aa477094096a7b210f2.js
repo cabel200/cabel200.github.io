1202845206d2583fc9676a00a1fb04560d714206
Caroline Abel <c_abel200@yahoo.com>
2021-08-15 21:13:09 -0400

    Updates

---
+++ precache-manifest.e9920f6bd44d2aa477094096a7b210f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1239e4252c79bb5a21c8da14bb07f7fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2442c08be2d05ab55f5",
+    "url": "/static/css/main.76664f89.chunk.css"
+  },
+  {
+    "revision": "dd2ff96ae70ec0a87d47",
+    "url": "/static/js/2.0fde14b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fde14b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2442c08be2d05ab55f5",
+    "url": "/static/js/main.c3e300f6.chunk.js"
+  },
+  {
+    "revision": "5de68cab1cc443f574b3",
+    "url": "/static/js/runtime-main.30ff6843.js"
+  }
+]);
