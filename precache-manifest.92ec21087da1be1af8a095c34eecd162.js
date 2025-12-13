9983a310bdc383cd4c6dde871cb212affa6b0070
CharlesOnesti <charlesonesti@college.harvard.edu>
2025-12-13 00:50:57 -0800

    Updates

---
+++ precache-manifest.92ec21087da1be1af8a095c34eecd162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8e3599c0773e90f74030ef7c99c27b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f35e9af56f36b070b1",
+    "url": "/static/css/main.76664f89.chunk.css"
+  },
+  {
+    "revision": "dd2ff96ae70ec0a87d47",
+    "url": "/static/js/2.0fde14b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0fde14b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f35e9af56f36b070b1",
+    "url": "/static/js/main.17acf6e7.chunk.js"
+  },
+  {
+    "revision": "5de68cab1cc443f574b3",
+    "url": "/static/js/runtime-main.30ff6843.js"
+  }
+]);
